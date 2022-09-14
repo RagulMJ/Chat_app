c31eb7e3e93b84e895c20e649923fcaee7c07b46
RagulMJ <98083899+RagulMJ@users.noreply.github.com>
2022-09-14 12:29:45 +0530

    chat app

--- src/components/TeamChannelList.jsx
+++ src/components/TeamChannelList.jsx
@@ -1,31 +1,42 @@
-import React from 'react'
-import { AddChannel } from '../assets'
+import React from 'react';
 
-const TeamChannelList = ({ children, error = false, loading, type }) => {
-    if (error) {
+import { AddChannel } from '../assets';
+
+const TeamChannelList = ({ setToggleContainer, children, error = false, loading, type, isCreating, setIsCreating, setCreateType, setIsEditing }) => {
+    if(error) {
         return type === 'team' ? (
-            <div className='team-channel-list'>
+            <div className="team-channel-list">
                 <p className="team-channel-list__message">
-                    connection error, please wait a moment and try again.
+                    Connection error, please wait a moment and try again.
                 </p>
             </div>
-        ) : null;
+        ) : null
     }
-    if (loading) {
+
+    if(loading) {
         return (
-            <div className='team-channel-list'>
+            <div className="team-channel-list">
                 <p className="team-channel-list__message loading">
-                    {type === 'team' ? 'channels' : 'Messages'} loading...
+                    {type === 'team' ? 'Channels' : 'Messages'} loading...
                 </p>
             </div>
         )
     }
-    return(
+
+    return (
         <div className="team-channel-list">
             <div className="team-channel-list__header">
-                <p className="team-channel-list__title">
+                <p className="team-channel-list__header__title">
                     {type === 'team' ? 'Channels' : 'Direct Messages'}
                 </p>
+                <AddChannel 
+                    isCreating={isCreating}
+                    setIsCreating={setIsCreating}
+                    setCreateType={setCreateType} 
+                    setIsEditing={setIsEditing}
+                    type={type === 'team' ? 'team' : 'messaging'}
+                    setToggleContainer={setToggleContainer}
+                />
             </div>
             {children}
         </div>
